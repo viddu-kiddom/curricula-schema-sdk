4a97f65392e4dabeed8910d4f03d80426bc48f0e
viddu-kiddom <161510737+viddu-kiddom@users.noreply.github.com>
2025-11-04 14:13:51 -0800

    Remove unused exporters `PostgresEAVExporter` and `SimpleTreeJsonExporter`

--- src/curricula_schema_sdk/exporters/stj_exporter.py
+++
@@ -1,8 +0,0 @@
-from treelib import Tree
-
-from curricula_schema_sdk.exporters.base_exporter import BaseExporter
-
-
-class SimpleTreeJsonExporter(BaseExporter):
-    def export(self, tree: Tree):
-        raise Exception("Not implemented yet")
