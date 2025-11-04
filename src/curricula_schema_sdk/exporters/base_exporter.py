1ff163128f97a606910fb9097133cd2f69908ae2
viddu-kiddom <161510737+viddu-kiddom@users.noreply.github.com>
2025-11-03 22:16:24 -0800

    Console exporter

--- src/curricula_schema_sdk/exporters/base_exporter.py
+++ src/curricula_schema_sdk/exporters/base_exporter.py
@@ -4,6 +4,17 @@ from treelib import Tree
 
 
 class BaseExporter(ABC):
+
     @abstractmethod
-    def export(self, tree: Tree):
+    def save_node(self, node) -> str:
         pass
+
+    @abstractmethod
+    def save_edge(self, parent_id, child_id, edge_type):
+        pass
+
+    def export(self, tree: Tree):
+        for node_id in tree.expand_tree():
+            node = tree[node_id]
+            rev_id = self.save_node(node)
+            self.save_edge(node.predecessor(tree.identifier), rev_id, "isParentOf")
